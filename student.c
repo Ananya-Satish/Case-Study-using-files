4daf63841eb3b164805cc959980e21409657db67
Ananya-Satish <165289696+Ananya-Satish@users.noreply.github.com>
2024-05-16 21:47:48 +0530

    Create student.c

---
+++ student.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "student.h"
+
+const char *subject_names[] = {"Physics", "PSWC", "Mathematics", "Electrical", "Mechanical"};
+
+void inputStudentData(struct Student *student, FILE *fp) {
+    student->name = (char *)malloc(50 * sizeof(char));
+    if (student->name == NULL) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter student name: ");
+    scanf("%s", student->name);
+
+    fprintf(fp, "%s ", student->name);
+
+    student->exam1_marks = (float *)malloc(5 * sizeof(float));
+    if (student->exam1_marks == NULL) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter marks for exam 1:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Enter marks for %s: ", subject_names[i]);
+        scanf("%f", &student->exam1_marks[i]);
+        fprintf(fp, "%.2f ", student->exam1_marks[i]);
+    }
+
+    student->exam2_marks = (float *)malloc(5 * sizeof(float));
+    if (student->exam2_marks == NULL) {
+        printf("Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter marks for exam 2:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Enter marks for %s: ", subject_names[i]);
+        scanf("%f", &student->exam2_marks[i]);
+        fprintf(fp, "%.2f ", student->exam2_marks[i]);
+    }
+    fprintf(fp, "\n");
+}
+
+void storeStudentData(struct Student *student) {
+    FILE *fp = fopen("student_data.txt", "a");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
+    inputStudentData(student, fp);
+    fclose(fp);
+}
+
+void searchStudentData(const char *name) {
+    FILE *fp = fopen("student_data.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char student_name[50];
+    float exam1_marks[5], exam2_marks[5];
+
+    while (fscanf(fp, "%s", student_name) != EOF) {
+        if (strcmp(student_name, name) == 0) {
+            printf("Name: %s\n", student_name);
+
+            for (int i = 0; i < 5; i++) {
+                fscanf(fp, "%f", &exam1_marks[i]);
+                printf("Exam 1 %s: %.2f\n", subject_names[i], exam1_marks[i]);
+            }
+
+            for (int i = 0; i < 5; i++) {
+                fscanf(fp, "%f", &exam2_marks[i]);
+                printf("Exam 2 %s: %.2f\n", subject_names[i], exam2_marks[i]);
+            }
+            break;
+        } else {
+            for (int i = 0; i < 10; i++) {
+                float dummy;
+                fscanf(fp, "%f", &dummy);
+            }
+        }
+    }
+
+    fclose(fp);
+}
